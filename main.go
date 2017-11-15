0982cb2ca993a7c9573ee97c784b43c1699634d2
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-11-16 02:04:27 +0900

    add server 2

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package go_tor_server
+package main
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main() {
+	// Echoのインスタンス作る
+	e := echo.New()
+
+	// ルーティング
+	e.GET("/", MainPage())
+
+	// サーバー起動
+	e.Start(":8080")
+}
+
+func MainPage() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	}
+}
